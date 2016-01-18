1cdc76d544e87397914b92f107397e1004ef191f
Justin Jung <justinjung9090@gmail.com>
2016-01-18 00:59:33 -0800

    signal implementation completed

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -7,29 +7,21 @@ export default class Home extends Component {
 	constructor() {
 		super();
 		this.signal = new Signal();
-		this.state = this.signal.getState();		
+		this.state = this.signal.getState();
+
+		this.setCount = (count) => {
+			this.setState({
+				count
+			});
+		};
 	}
 
 	componentDidMount() {
-		// let setCount = (count) => {
-		// 	this.setState({
-		// 		count
-		// 	});
-		// };
-
-		this.signal.event.count.add(this.setCount.bind(this), this);
-
-		console.log(this.signal.event.count.has(this.setCount.bind(this), this));
+		this.signal.event.count.add(this.setCount);
 	}
 
 	componentWillUnmount() {
-		
-	}
-
-	setCount(count) {
-		this.setState({
-			count
-		});
+		this.signal.event.count.remove(this.setCount);
 	}
 	
 	incrementCount() {
@@ -46,7 +38,7 @@ export default class Home extends Component {
 				<h1>Home page</h1>
 				<p>This is a home page</p>
 				<img className='logo' src={require('../../assets/logo_tb.png')} />
-				<h1>Counter</h1>
+				<h1>Home Counter</h1>
 				<p>{this.state.count}</p>
 				<button onClick={this.incrementCount.bind(this)}>+</button>
 				<button onClick={this.decrementCount.bind(this)}>-</button>
--- src/components/page1/page1.js
+++ src/components/page1/page1.js
@@ -1,10 +1,35 @@
 import './page1.scss';
 
 import React, { Component } from 'react';
+import Signal from '../../signal/signal';
 
 export default class Page1 extends Component {
 	constructor() {
 		super();
+		this.signal = new Signal();
+		this.state = this.signal.getState();
+
+		this.setCount = (count) => {
+			this.setState({
+				count
+			});
+		};
+	}
+
+	componentDidMount() {
+		this.signal.event.count.add(this.setCount);
+	}
+
+	componentWillUnmount() {
+		this.signal.event.count.remove(this.setCount);
+	}
+	
+	incrementCount() {
+		this.signal.incrementCount();
+	}
+
+	decrementCount() {
+		this.signal.decrementCount();
 	}
 
 	render() {
@@ -12,6 +37,10 @@ export default class Page1 extends Component {
 			<div className={'content page1 ' + this.props.device}>
 				<h1>Page 1</h1>
 				<p>This is page 1</p>
+				<h1>Page 1 Counter</h1>
+				<p>{this.state.count}</p>
+				<button onClick={this.incrementCount.bind(this)}>+</button>
+				<button onClick={this.decrementCount.bind(this)}>-</button>
 			</div>
 		);
 	}
