6fa6b22fcfbcfd4edba8b392f4a91edc96e540ad
Justin Jung <justin.jung@thinkingbox.ca>
2016-05-13 00:44:37 -0700

    updated songs.js

--- src/constants/songs.js
+++ src/constants/songs.js
@@ -1,157 +1,32 @@
 const Hymns = [
 	{
-		title: '우리 주 예수 그리스도 *',
-		track: '33',
+		title: '오소서 성령이여 *',
+		track: '142',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
-		title: '나는 포도나무요',
-		track: '35',
+		title: '주의 얼이 *',
+		track: '144',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
-		title: '평화를 주옵소서',
-		track: '44',
+		title: '임하소서 성령이여 *',
+		track: '145',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
-		title: '사랑의 송가',
-		track: '46',
+		title: '임하소서 성령이여 *',
+		track: '147',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
-		title: '착하신 목자',
-		track: '55',
+		title: '모든 은혜 근원이신 성령 *',
+		track: '150',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
-		title: '목자를 따라서',
-		track: '56',
-		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
-	},
-	{
-		title: '형제여 기뻐하라 알렐루야',
-		track: '128',
-		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
-	},
-	{
-		title: '알렐루야 노래하자',
-		track: '129',
-		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
-	},
-	{
-		title: '찬미 노래 드리자',
-		track: '131',
-		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
-	},
-	{
-		title: '알렐루야 알렐루야 주 예수',
-		track: '135',
-		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
-	},
-	{
-		title: '부활하신 주 예수 *',
-		track: '137',
-		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
-	},
-	{
-		title: '죽음을 이긴 *',
-		track: '141',
-		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
-	},
-	{
-		title: '하느님의 어린양 *',
-		track: '160',
-		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
-	},
-	{
-		title: '떡과 술의 형상에 *',
-		track: '164',
-		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
-	},
-	{
-		title: '주의 잔',
+		title: '주의 잔치 *',
 		track: '165',
-		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
-	},
-	{
-		title: '사랑의 주여 오소서',
-		track: '173',
-		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
-	},
-	{
-		title: '사랑의 신비',
-		track: '174',
-		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
-	},
-	{
-		title: '이보다 더 큰 은혜와',
-		track: '175',
-		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
-	},
-	{
-		title: '나의 생명 드리니',
-		track: '210',
-		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
-	},
-	{
-		title: '정성 어린 우리 제물 *',
-		track: '217',
-		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
-	},
-	{
-		title: '생활한 제물',
-		track: '220',
-		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
-	},
-	{
-		title: '사랑하올 어머니 *',
-		track: '236',
-		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
-	},
-	{
-		title: '거룩한 어머니 *',
-		track: '239',
-		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
-	},
-	{
-		title: '성모의 성월',
-		track: '244',
-		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
-	},
-	{
-		title: '로사리오의 기도 *',
-		track: '270',
-		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
-	},
-	{
-		title: '부드러운 주의 손이',
-		track: '439',
-		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
-	},
-	{
-		title: '주님의 발자국 아는가',
-		track: '440',
-		voices: [ 'soprano', 'bass' ]
-	},
-	{
-		title: '주님은 우리 사랑하셨네',
-		track: '496',
-		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
-	},
-	{
-		title: '만찬의 신비',
-		track: '499',
-		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
-	},
-	{
-		title: '받으소서 우리 마음',
-		track: '501',
-		voices: [ ]
-	},
-	{
-		title: '미약하온 우리 제물',
-		track: '511',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	}
 ];
