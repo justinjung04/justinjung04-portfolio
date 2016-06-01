78be5e8abe6c3e0019136997b8131e87d822cd8c
Justin Jung <justinjung04@gmail.com>
2016-06-01 02:32:21 -0700

    updated songs

--- src/constants/songs.js
+++ src/constants/songs.js
@@ -1,42 +1,42 @@
 const Hymns = [
 	{
-		title: '[입당] 사랑의 성사',
-		track: '169',
+		title: '[입당] 구세주의 성심이여',
+		track: '202',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
-		title: '[봉헌1] 봉헌',
-		track: '332',
+		title: '[봉헌1] 나는 세상의 빛입니다',
+		track: '421',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
-		title: '[봉헌2] 주님께 올리는 기도',
-		track: '510',
+		title: '[봉헌2] 주 나의 목자 되시니',
+		track: '51',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
-		title: '[봉헌3] 정성어린 우리 제물',
-		track: '217',
+		title: '[봉헌3] 주여 우리는 지금',
+		track: '512',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
-		title: '[성체1] 사랑의 성체성사',
-		track: '334',
+		title: '[성체1] 주여 임하소서',
+		track: '151',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
-		title: '[성체2] 생명의 성체여',
-		track: '163',
+		title: '[성체2] 자애로운 예수',
+		track: '170',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
-		title: '[성체3] 오묘하온 성체',
-		track: '168',
+		title: '[성체3] 주의 사랑 전하리',
+		track: '179',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
-		title: '[파견] 맑은 하늘 오월은',
-		track: '245',
+		title: '[파견] 주 하느님 크시도다',
+		track: '2',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
@@ -80,6 +80,11 @@ const Hymns = [
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
+		title: '생명의 성체여',
+		track: '163',
+		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
+	},
+	{
 		title: '주의 잔치',
 		track: '165',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
@@ -90,8 +95,13 @@ const Hymns = [
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
-		title: '자애로운 예수',
-		track: '170',
+		title: '오묘하온 성체',
+		track: '168',
+		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
+	},
+	{
+		title: '사랑의 성사',
+		track: '169',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
@@ -100,13 +110,33 @@ const Hymns = [
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
-		title: '미약하온 우리 제물',
-		track: '511',
+		title: '정성어린 우리 제물',
+		track: '217',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
-		title: '주여 우리는 지금',
-		track: '512',
+		title: '맑은 하늘 오월은',
+		track: '245',
+		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
+	},
+	{
+		title: '봉헌',
+		track: '332',
+		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
+	},
+	{
+		title: '사랑의 성체성사',
+		track: '334',
+		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
+	},
+	{
+		title: '주님께 올리는 기도',
+		track: '510',
+		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
+	},
+	{
+		title: '미약하온 우리 제물',
+		track: '511',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	}
 ];
