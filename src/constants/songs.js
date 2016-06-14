a7f71d7484e867810ccbb517d31f19ab631f0065
Justin Jung <justinjung04@gmail.com>
2016-06-13 22:10:40 -0700

    updated songs

--- src/constants/songs.js
+++ src/constants/songs.js
@@ -1,47 +1,57 @@
 const Hymns = [
 	{
-		title: '[입당] 구세주의 성심이여',
-		track: '202',
+		title: '[입당] 둘이나 셋이 모인곳에',
+		track: '456',
+		voices: []
+	},
+	{
+		title: '[봉헌1] 평화를 주옵소서',
+		track: '44',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
-		title: '[봉헌1] 나는 세상의 빛입니다',
-		track: '421',
+		title: '[봉헌2] 정성어린 우리 제물',
+		track: '217',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
-		title: '[봉헌2] 주 나의 목자 되시니',
-		track: '51',
+		title: '[봉헌3] 십자가에 제헌되신',
+		track: '216',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
-		title: '[봉헌3] 주여 우리는 지금',
-		track: '512',
+		title: '[성체1] 주님 안에 하나',
+		track: '399',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
-		title: '[성체1] 주여 임하소서',
-		track: '151',
+		title: '[성체2] 오 거룩한 마음',
+		track: '203',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
-		title: '[성체2] 자애로운 예수',
-		track: '170',
+		title: '[성체3] 주의 빵을 서로 나누세',
+		track: '502',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
-		title: '[성체3] 주의 사랑 전하리',
-		track: '179',
+		title: '[파견] 하나 되게 하소서',
+		track: '39',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
-		title: '[파견] 주 하느님 크시도다',
+		title: '주 하느님 크시도다',
 		track: '2',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
 		title: '사랑의 하느님',
 		track: '25',
+		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
+	},
+	{
+		title: '주 나의 목자 되시니',
+		track: '51',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
@@ -80,6 +90,11 @@ const Hymns = [
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
+		title: '주여 임하소서',
+		track: '151',
+		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
+	},
+	{
 		title: '생명의 성체여',
 		track: '163',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
@@ -105,13 +120,23 @@ const Hymns = [
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
+		title: '자애로운 예수',
+		track: '170',
+		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
+	},
+	{
 		title: '믿음 소망 사랑',
 		track: '176',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
-		title: '정성어린 우리 제물',
-		track: '217',
+		title: '주의 사랑 전하리',
+		track: '179',
+		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
+	},
+	{
+		title: '구세주의 성심이여',
+		track: '202',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
@@ -130,6 +155,11 @@ const Hymns = [
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	},
 	{
+		title: '나는 세상의 빛입니다',
+		track: '421',
+		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
+	},
+	{
 		title: '주님께 올리는 기도',
 		track: '510',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
@@ -137,6 +167,11 @@ const Hymns = [
 	{
 		title: '미약하온 우리 제물',
 		track: '511',
+		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
+	},
+	{
+		title: '주여 우리는 지금',
+		track: '512',
 		voices: [ 'soprano', 'alto', 'tenor', 'bass' ]
 	}
 ];
